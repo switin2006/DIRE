fded88705790633ad43824fd3befb7317471e5d0
switin2006 <switinmechineni@gmail.com>
2025-10-02 12:46:14 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,12 @@
+%%writefile train_cifake.sh
 #!/bin/bash
-### make sure that you have modified the EXP_NAME, DATASETS, DATASETS_TEST
-eval "$(conda shell.bash hook)"
-conda activate dire
-EXP_NAME="lsun_adm"
-DATASETS="lsun_adm"
-DATASETS_TEST="lsun_adm"
-python train.py --gpus 0 --exp_name $EXP_NAME datasets $DATASETS datasets_test $DATASETS_TEST
+EXP_NAME="cifake_classifier"
+DATASETS="cifake"
+DATASETS_TEST="cifake" # This is used for validation during training
+
+# Create a symbolic link so the script can find the data at the expected 'data' path
+# The train.py script looks for a folder named 'data' in the root project directory.
+ln -sfn /content/data_DIRE /content/DIRE/data
+
+# Run training on a single GPU (gpus 0)
+python /content/DIRE/train.py --gpus 0 --exp_name $EXP_NAME datasets $DATASETS datasets_test $DATASETS_TEST
