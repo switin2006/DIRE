388d28a5f42571b3f56ba90d56a8757028bd92c5
switin2006 <switinmechineni@gmail.com>
2025-10-02 12:45:16 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
+%%writefile test_cifake.sh
 #!/bin/bash
-### make sure that you have modified the EXP_NAME, CKPT, DATASETS_TEST
-eval "$(conda shell.bash hook)"
-conda activate dire
+EXP_NAME="cifake_classifier_eval"
+# This path points to the 'best' model saved by the training script in the previous step.
+CKPT_PATH="/content/DIRE/data/exp/cifake_classifier/ckpt/model_epoch_best.pth"
+DATASETS_TEST="cifake"
 
-EXP_NAME="lsun_adm_release"
-CKPT="/data3/wangzd/MSRA/result/guofeng-Midv5-450total-2e-6-rank64/1400/lsun_adm.pth"
-DATASETS_TEST="release"
-python test.py --gpus 0 --ckpt $CKPT --exp_name $EXP_NAME datasets_test $DATASETS_TEST
+# Ensure the symbolic link to the data folder exists
+ln -sfn /content/data_DIRE /content/DIRE/data
+
+# Run evaluation on a single GPU (gpus 0)
+python /content/DIRE/test.py --gpus 0 --ckpt $CKPT_PATH --exp_name $EXP_NAME datasets_test $DATASETS_TEST
